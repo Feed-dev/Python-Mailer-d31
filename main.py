ea7193c0dea53de6e28b4a0e0f00c6c7189eeeb9
federico Lubbert <federico.lubbert@gmail.com>
2024-03-08 18:58:42 +0100

    init

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import smtplib
 import datetime as dt
+import random
 
 my_email = ""
 my_password = ""
-
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=my_password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="",
-        msg="Subject:Hello\n\nThis is the body of my email."
-    )
 
 now = dt.datetime.now()
 year = now.year
 month = now.month
 day_of_week = now.weekday()
 
-date_of_birth = dt.datetime(year=1995, month=5, day=15, hour=4)
+if day_of_week == 0:
+    # open a file quotes.txt
+    with open("quotes.txt") as file:
+        contents = file.readlines()
+        quote = random.choice(contents)
 
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="",
+            msg="Subject:Monday Motivation\n\n{quote}"
+        )
+        print("Email sent successfully")
